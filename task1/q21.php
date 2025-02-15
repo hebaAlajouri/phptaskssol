323efca3c0649669820da52c2783a5d8eccf893d
hebaAlajouri <hebaajoury1212@gmail.com>
2025-02-15 15:22:37 +0300

    Delete q21.php

--- task1/q21.php
+++
@@ -1,8 +0,0 @@
-<?php
-$numericString='085119';
-$hours= substr($numericString,0,2);
-$minutes= substr($numericString,2,4);
-$seconds= substr($numericString,4,6);
-$formattedTime=$houres . ':' . $minutes . ':' . $seconds;
-echo $formattedTime;
-?>
