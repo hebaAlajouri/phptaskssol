b57a7a0d8c1b3669b5bca72d35fc767975d2bcac
hebaAlajouri <hebaajoury1212@gmail.com>
2025-02-15 15:22:27 +0300

    Delete q24.php

--- task1/q24.php
+++
@@ -1,6 +0,0 @@
-<?php
-$characters='1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-$shuffled=str_shuffle($characters);
-$password=substr($shuffled,0,8);
-echo "Generated Password:" . $password;
-?>
